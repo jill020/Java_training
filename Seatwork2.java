9c0756aef94daef82022ab4fdf406a846fcd06a4
Jillian Vitalico <gstjvitalico@robinsonsbank.com.ph>
2025-12-04 16:13:04 +0800

    Module 2 Activity 2

---
+++ Seatwork2.java
@@ -0,0 +1,9 @@
+package com.bpi.hello;
+
+public class Seatwork2 {
+	public static void main(String[] args) {
+        WhiteBoard wb = new WhiteBoard();
+        int result = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + String.valueOf(result));
+    }
+}
