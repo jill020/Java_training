ed552d628a6e4d31eb01df080ae040018920e440
Jillian Vitalico <gstjvitalico@robinsonsbank.com.ph>
2025-12-09 16:31:56 +0800

    M2_Activity_5

---
+++ MSWord.java
@@ -0,0 +1,20 @@
+package com.bpi.oop5_1;
+
+public class MSWord extends Program{
+	
+	public MSWord(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");
+		this.setIsRunning(true);
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+		this.setIsRunning(false);
+	}
+}
---
+++ Program.java
@@ -0,0 +1,36 @@
+package com.bpi.oop5_1;
+
+public abstract class Program {
+
+	private String name;
+	private boolean isRunning = false;
+	
+	public Program() {
+
+	}
+
+	public Program(String name) {
+		this.name = name;
+	}
+
+	abstract void run();
+
+	abstract void stop();
+
+	public String getName() {
+		return name;
+	};
+
+	public void setName(String name) {
+		this.name = name;
+	};
+
+	public boolean getIsRunning() {
+		return isRunning;
+	};
+
+	public void setIsRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	};
+
+}
