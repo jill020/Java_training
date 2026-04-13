34ce932ecd4f31c965e06f8bd5b15852be2f6682
Jillian Vitalico <gstjvitalico@robinsonsbank.com.ph>
2026-04-13 13:07:56 +0800

    Actvity 1

---
+++ src/main/java/com/bpi/activity1/Module8Application.java
@@ -0,0 +1,13 @@
+package com.bpi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module8Application.class, args);
+	}
+
+}
